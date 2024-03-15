536a868ddd3e00cf149c9ad203730be21cd66cf1
lpoiret <lpoiret@kaliop.com>
2024-03-15 15:31:17 +0100

    Initial commit

---
+++ src/basics/constants/tasks.const.ts
@@ -0,0 +1,25 @@
+import { TaskType } from "../types/task.type";
+
+
+export const TASKS: TaskType[] = [
+    {
+        "id": "1",
+        "name": "Classic Margherita Pizza",
+    },
+    {
+        "id": "2",
+        "name": "Vegetarian Stir-Fry",
+    },
+    {
+        "id": "3",
+        "name": "Chocolate Chip Cookies",
+    },
+    {
+        "id": "4",
+        "name": "Chicken Alfredo Pasta",
+    },
+    {
+        "id": "5",
+        "name": "Mango Salsa Chicken",
+    }
+]
