f02ec470a94364bd8dcaa39f188666df0303155f
Matthieu LINCONNU <mlinconnu@kaliop.com>
2024-04-12 14:39:41 +0200

    Exercice 1

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,16 @@
-//TODO: Todolist functionnal component
+import { TASKS } from "../../basics/constants/tasks.const";
+
+const TodoList = () => {
+  return (
+    <>
+      <h1>My Todo</h1>
+      <ul>
+        {TASKS.map((task) => {
+          return <li key={task.id}>{task.name}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default TodoList;
