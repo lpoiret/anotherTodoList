35a6ea81d13863889457e2cbcd5d700a9f884b6b
Matthieu LINCONNU <mlinconnu@kaliop.com>
2024-04-19 14:53:23 +0200

    Exercice 2

---
+++ src/components/TodoList/TodoList.hook.ts
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { TASKS } from "../../basics/constants/tasks.const";
+import { v4 as uuidv4 } from 'uuid';
+
+export const useTodoList = () => {
+  const [todoListItems, setTodoListItems] = useState(
+    TASKS.map((task) => ({ name: task.name, id: task.id }))
+  );
+
+  const generateTodoItem = () => {
+    const randomTask =
+    TASKS[Math.floor(Math.random() * (TASKS.length - 1))];
+
+    return {name: randomTask.name, id: uuidv4()};
+  };
+
+  const addTodoItem = () => {
+    // Shallow
+    // todoListItems.push(generateTodoItem())
+    // setTodoListItems(todoListItems)
+
+    // Prev state
+    // setTodoListItems([...todoListItems, generateTodoItem()]);
+
+    setTodoListItems((prevState) => [...prevState, generateTodoItem()]);
+  };
+
+  return {
+    todoListItems,
+    generateTodoItem,
+    addTodoItem,
+  };
+};
