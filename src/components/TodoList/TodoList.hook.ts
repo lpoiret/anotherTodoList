051f26fdd7ef8932d78053323897544c73d66a37
Matthieu LINCONNU <mlinconnu@kaliop.com>
2024-04-19 14:53:23 +0200

    Exercice 2

---
+++ src/components/TodoList/TodoList.hook.ts
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { RECIPES } from "../../basics/constants/recipes.const";
+import { v4 as uuidv4 } from 'uuid';
+
+export const useTodoList = () => {
+  const [todoListItems, setTodoListItems] = useState(
+    RECIPES.map((recipe) => ({ name: recipe.name, id: recipe.id }))
+  );
+
+  const generateTodoItem = () => {
+    const randomRecipe =
+      RECIPES[Math.floor(Math.random() * (RECIPES.length - 1))];
+
+    return {name: randomRecipe.name, id: uuidv4()};
+  };
+
+  const addTodoItem = () => {
+    // Shallow
+    // todoListItems.push(generateTodoItem())
+    // setTodoListItems(todoListItems)
+
+    // Prev state
+    // setTodoListItems([...todoListItems, generateTodoItem()]);
+
+    setTodoListItems((prevState) => [...prevState, generateTodoItem()]);
+  };
+
+  return {
+    todoListItems,
+    generateTodoItem,
+    addTodoItem,
+  };
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,11 +1,17 @@
-import { RECIPES } from "../../basics/constants/recipes.const";
+import { Button } from "@mui/material";
+import { useTodoList } from "./TodoList.hook";
 
 const TodoList = () => {
+  const { addTodoItem, todoListItems } = useTodoList();
+
   return (
     <>
       <h1>My Todo</h1>
+      <Button variant="outlined" onClick={addTodoItem}>
+        Add
+      </Button>
       <ul>
-        {RECIPES.map((recipe) => {
+        {todoListItems.map((recipe) => {
           return <li key={recipe.id}>{recipe.name}</li>;
         })}
       </ul>
