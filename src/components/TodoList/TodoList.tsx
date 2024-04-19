2bc099fd5f700831e1ace7cce3e322ddca5f7507
Matthieu LINCONNU <mlinconnu@kaliop.com>
2024-04-19 14:53:23 +0200

    Exercice 2

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,11 +1,17 @@
-import { TASKS } from "../../basics/constants/tasks.const";
+import { Button } from "@mui/material";
+import { useTodoList } from "./TodoList.hook";
 
 const TodoList = () => {
+  const { addTodoItem, todoListItems } = useTodoList();
+
   return (
     <>
       <h1>My Todo</h1>
+      <Button variant="outlined" onClick={addTodoItem}>
+        Add
+      </Button>
       <ul>
-        {TASKS.map((task) => {
+        {todoListItems.map((task) => {
           return <li key={task.id}>{task.name}</li>;
         })}
       </ul>
