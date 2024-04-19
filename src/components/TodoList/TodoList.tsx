c0b424751e28f8be04545843527e6ed4a0543d44
Matthieu LINCONNU <mlinconnu@kaliop.com>
2024-04-19 15:52:04 +0200

    Exercice 4

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,25 +1,36 @@
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { useTodoList } from "./TodoList.hook";
 
 const TodoList = () => {
-  const { addTodoItem, removeTodoItem, todoListItems } = useTodoList();
+  const { addTodoItem, removeTodoItem, todoListItems, inputRef } =
+    useTodoList();
 
   return (
     <>
       <h1>My Todo</h1>
-      <Button variant="outlined" onClick={addTodoItem}>
-        Add
-      </Button>
+      <form onSubmit={addTodoItem}>
+        <TextField
+          id="task"
+          label="Task"
+          variant="outlined"
+          size="small"
+          inputRef={inputRef}
+        />
+        <Button variant="outlined" type="submit" sx={{ marginLeft: "20px" }}>
+          Add
+        </Button>
+      </form>
 
       <ul>
         {todoListItems.length > 0 ? (
           todoListItems.map((task) => {
             return (
-              <li key={task.id}>
+              <li key={task.id} style={{marginTop: "10px"}}>
                 {task.name}
                 <Button
-                  sx={{marginLeft: "20px"}}
+                  sx={{ marginLeft: "20px" }}
                   variant="outlined"
+                  size="small"
                   onClick={() => removeTodoItem(task.id)}
                 >
                   Remove
