aff287e4c10f7084b5b4e2d1e4c71b273bd74c1d
Laurent POIRET <lpoiret@kaliop.com>
2024-07-05 14:43:11 +0200

    Exercice 5

--- src/components/TodoList/TodoList.hook.ts
+++ src/components/TodoList/TodoList.hook.ts
@@ -1,12 +1,13 @@
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useMemo, useRef, useState } from "react";
 import { TASKS } from "../../basics/constants/tasks.const";
 import { v4 as uuidv4 } from 'uuid';
 import { TaskType } from "../../basics/types/task.type";
 
 export const useTodoList = () => {
   const [todoListItems, setTodoListItems] = useState<TaskType[]>(
-    TASKS.map((task) => ({ name: task.name, id: task.id, done: false }))
+    TASKS.map((task) => ({ name: task.name, id: task.id, done: task.done }))
   );
+
   const inputRef = useRef<HTMLInputElement>();
 
   const generateTodoItem = (taskName: string): TaskType => {
@@ -14,16 +15,40 @@ export const useTodoList = () => {
   };
 
   const addTodoItem = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    
-    const inputValue = inputRef.current?.value
-    if(inputValue) {
-      setTodoListItems((prevState) => [...prevState, generateTodoItem(inputValue)]);
+    event.preventDefault();
+
+    const inputValue = inputRef.current?.value;
+    if (inputValue) {
+      setTodoListItems((prevState) => [
+        ...prevState,
+        generateTodoItem(inputValue),
+      ]);
     }
   };
 
   const removeTodoItem = (id: string) => {
     setTodoListItems((prevState) => prevState.filter((item) => item.id != id));
+  };
+
+  const todoList = useMemo(
+    () => todoListItems.filter((task) => !task.done),
+    [todoListItems]
+  );
+  const doneList = useMemo(
+    () => todoListItems.filter((task) => task.done),
+    [todoListItems]
+  );
+
+  const onChecked = (taskId: string) => {
+    const taskIndex = todoListItems.findIndex((task) => task.id === taskId);
+
+    if (taskIndex === -1) {
+      return;
+    }
+    const task = todoListItems[taskIndex];
+    task.done = !task.done;
+    todoListItems.splice(taskIndex, 1, task);
+    setTodoListItems([...todoListItems]);
   };
 
   return {
@@ -32,5 +57,8 @@ export const useTodoList = () => {
     addTodoItem,
     removeTodoItem,
     inputRef,
+    onChecked,
+    todoList,
+    doneList,
   };
 };
