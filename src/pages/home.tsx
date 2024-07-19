d696ed184eef1a608209c79285f7b381a760a73d
Matthieu LINCONNU <mlinconnu@kaliop.com>
2024-07-19 15:16:57 +0200

    Exercice 7

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+
+function HomePage() {
+  return (
+    <Container maxWidth="sm">
+      <h1>Home</h1>
+    </Container>
+  );
+}
+
+export default HomePage;
